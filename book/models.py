b50591a3f3d6c3120bbffd48d1a80a29525f5c5a
zipeta <zipetanabz@gmail.com>
2020-01-31 15:26:53 +0300

    Fisrt Commit

---
+++ book/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+#Models
+
+class Book(models.Model):
+    name = models.CharField(max_length = 50)
+    picture = models.ImageField()
+    author = models.CharField(max_length = 30, default='anonymous')
+    email = models.EmailField(blank = True)
+    describe = models.TextField(default = 'DataFlair')
+
+    def __str__(self):
+        return self.name
